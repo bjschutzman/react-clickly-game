3fcc88f867eb1e5e94d763040391e41a0684dcf6
Brian Schutzman <bjschutzman@gmail.com>
2019-07-22 21:03:53 -0500

    Updates

---
+++ precache-manifest.fd11901ca1dc6f7e6760d634f034f6d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ade2ffb90d126f90931b3af234c91a",
+    "url": "/react-clickly-game/index.html"
+  },
+  {
+    "revision": "a8aff376b3d76b1c8892",
+    "url": "/react-clickly-game/static/css/main.5c238757.chunk.css"
+  },
+  {
+    "revision": "4dbc779420a3e2162ebe",
+    "url": "/react-clickly-game/static/js/2.9590dc6d.chunk.js"
+  },
+  {
+    "revision": "a8aff376b3d76b1c8892",
+    "url": "/react-clickly-game/static/js/main.49a497a3.chunk.js"
+  },
+  {
+    "revision": "3fe78e8196d2a7b4c353",
+    "url": "/react-clickly-game/static/js/runtime~main.9da8d3ff.js"
+  }
+]);
